e1f6e90954f3ada80e3aa545fcf6f91f714c8329
jamin12 <55321248+jamin12@users.noreply.github.com>
2024-06-25 21:21:54 +0900

    feat: init

---
+++ src/main/java/ai/jamin/hitcount/HitCountApplication.java
@@ -0,0 +1,13 @@
+package ai.jamin.hitcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitCountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/jamin/hitcount/HitCountApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.jamin.hitcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitCountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
